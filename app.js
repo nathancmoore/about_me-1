0eade77a1748e06b5ff5aa61b6c0f6e93d051332
allstot <n.allstot@gmail.com>
2017-08-08 16:25:29 -0700

    Moved JS to app.js, where it belongs and corrceted error in logic

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+var eatsVeggie = prompt('Does Noah eat vegetables?');
+
+if (eatsVeggie === 'yes' || eatsVeggie === 'y') {
+  alert('WRONG - Noah does not eat vegetables');
+}
+else if (eatsVeggie === 'N' || eatsVeggie === 'n') {
+  alert('Correct!! - Noah does not eat vegetables');
+}
+else {
+  alert('You did not enter a Yes/No/Y/N');
+}
+console.log('Question Prompt Does Noah eat vegetables?');
+console.log('Entered vaule was ' + eyeColor);
