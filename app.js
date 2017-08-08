4033a1aedf3294593d621a9590521b85b6e922fa
allstot <n.allstot@gmail.com>
2017-08-08 16:48:50 -0700

    Added Q2 Logic

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ if (lowerEatsVeggie === 'yes' || lowerEatsVeggie === 'y') {
   alert('WRONG - Noah does not eat vegetables');
 }
 else if (lowerEatsVeggie === 'no' || lowerEatsVeggie === 'n') {
-  alert('Correct!! - Noah does not eat vegetables');
+  alert('Correct!! - Noah does not eat vegetables.');
+}
+else {
+  alert('You did not enter a  Yes, No, Y or N');
+}
+
+var eatsMeat = prompt('Using Yes, No, Y or N anwser the following: Does Noah eat meat?');
+var lowerEatsMeat = eatsMeat.toLowerCase ();
+console.log('Question Prompt Does Noah eat meat?');
+console.log('Entered vaule is ' + eatsMeat);
+console.log('Converted vaule is ' + lowerEatsMeat);
+
+if (lowerEatsMeat === 'no' || lowerEatsMeat === 'n') {
+  alert('WRONG - Noah does eat meat.');
+}
+else if (lowerEatsMeat === 'yes' || lowerEatsMeat === 'y') {
+  alert('Correct!! - Noah does eat meat.');
 }
 else {
   alert('You did not enter a  Yes, No, Y or N');
