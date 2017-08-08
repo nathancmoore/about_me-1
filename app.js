8190ef804f0fbdb93838885521eebe99c056a599
allstot <n.allstot@gmail.com>
2017-08-08 16:40:12 -0700

    Correct logic - Yes it works now - for q1

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var eatsVeggie = prompt('Does Noah eat vegetables?');
+var eatsVeggie = prompt('Using Yes, No, Y or N anwser the following: Does Noah eat vegetables?');
+var lowerEatsVeggie = eatsVeggie.toLowerCase ();
+console.log('Question Prompt Does Noah eat vegetables?');
+console.log('Entered vaule is ' + eatsVeggie);
+console.log('Converted vaule is ' + lowerEatsVeggie);
 
-if (eatsVeggie === 'yes' || eatsVeggie === 'y') {
+if (lowerEatsVeggie === 'yes' || lowerEatsVeggie === 'y') {
   alert('WRONG - Noah does not eat vegetables');
 }
-else if (eatsVeggie === 'N' || eatsVeggie === 'n') {
+else if (lowerEatsVeggie === 'no' || lowerEatsVeggie === 'n') {
   alert('Correct!! - Noah does not eat vegetables');
 }
 else {
-  alert('You did not enter a Yes/No/Y/N');
+  alert('You did not enter a  Yes, No, Y or N');
 }
-console.log('Question Prompt Does Noah eat vegetables?');
-console.log('Entered vaule was ' + eyeColor);
