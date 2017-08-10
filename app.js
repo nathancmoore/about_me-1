7e1016f5e5d9ced277d72454e2ce918b5b4aaa58
Nathan Moore <ncmooore1986@gmail.com>
2017-08-10 15:04:37 -0700

    added functions

--- app.js
+++ app.js
@@ -24,46 +24,50 @@ noInc = [1, 0, 1, 0, 1];
 var yesInc;
 yesInc = [0, 1, 0, 1, 0];
 
-for (var qcount = 0; qcount < 5; qcount++) {
-  var tempReply = prompt (usrName + ' - ' + question[qcount] + ' ' + foods[qcount]);
+function preQa() {
+  for (var qcount = 0; qcount < 5; qcount++) {
+    var tempReply = prompt (usrName + ' - ' + question[qcount] + ' ' + foods[qcount]);
 
-  console.log(tempReply);
-  console.log(tempReply.toLowerCase() );
+    console.log(tempReply);
+    console.log(tempReply.toLowerCase() );
 
-  if (tempReply.toLowerCase() === 'yes' || tempReply.toLowerCase() === 'y') {
-    alert(yesRespo[qcount] + ' ' + foods[qcount]);
-    correctCount += yesInc[qcount];
-  }
-  else if (tempReply.toLowerCase() === 'no' || tempReply.toLowerCase() === 'n') {
-    alert(noRespo[qcount] + ' ' + foods[qcount]);
-    correctCount += noInc[qcount];
-  }
-  else {
-    alert(failRespo[qcount]);
+    if (tempReply.toLowerCase() === 'yes' || tempReply.toLowerCase() === 'y') {
+      alert(yesRespo[qcount] + ' ' + foods[qcount]);
+      correctCount += yesInc[qcount];
+    }
+    else if (tempReply.toLowerCase() === 'no' || tempReply.toLowerCase() === 'n') {
+      alert(noRespo[qcount] + ' ' + foods[qcount]);
+      correctCount += noInc[qcount];
+    }
+    else {
+      alert(failRespo[qcount]);
+    }
   }
 }
 
 var theNum = 76;
 
-for (var guessCount = 1; guessCount < 5; guessCount++) {
-  var numGuess = prompt (usrName + ' - Pick a number: 1 to 100 - ' + guessCount + ' of 4 attempts.');
+function guessNum() {
+  for (var guessCount = 1; guessCount < 5; guessCount++) {
+    var numGuess = prompt (usrName + ' - Pick a number: 1 to 100 - ' + guessCount + ' of 4 attempts.');
 
-  console.log('Guess count' + guessCount);
-  console.log('Guess ' + numGuess);
+    console.log('Guess count' + guessCount);
+    console.log('Guess ' + numGuess);
 
-  if (numGuess == theNum) {
-    alert('Correct!!');
-    guessCount = 5;
-    correctCount++;
-  }
-  else if (numGuess < 76) {
-    alert('Incorrect - Pick a larger number.');
-  }
-  else if (numGuess > 76) {
-    alert('Incorrect - Pick a smaller number.');
-  }
-  else {
-    alert('Your guess did not meet the number requirement - You lose a turn.');
+    if (numGuess == theNum) {
+      alert('Correct!!');
+      guessCount = 5;
+      correctCount++;
+    }
+    else if (numGuess < 76) {
+      alert('Incorrect - Pick a larger number.');
+    }
+    else if (numGuess > 76) {
+      alert('Incorrect - Pick a smaller number.');
+    }
+    else {
+      alert('Your guess did not meet the number requirement - You lose a turn.');
+    }
   }
 }
 
@@ -72,25 +76,31 @@ catNames = ['tommy', 'tommie', 'tom', 'thom'];
 
 var catCorrectCount = 0;
 
-for (var catGuessCount = 1; catGuessCount < 7; catGuessCount++) {
-  var nameGuess = prompt (usrName + ' - Pick the names of the past and present cats of Noah  - ' + catGuessCount + ' of 6 attempts.');
+function guessCat() {
+  for (var catGuessCount = 1; catGuessCount < 7; catGuessCount++) {
+    var nameGuess = prompt (usrName + ' - Pick the names of the past and present cats of Noah  - ' + catGuessCount + ' of 6 attempts.');
 
-  console.log('Guess count' + catGuessCount);
-  console.log('Guess ' + nameGuess);
+    console.log('Guess count' + catGuessCount);
+    console.log('Guess ' + nameGuess);
 
-  if (catNames.includes(nameGuess.toLowerCase())) {
-    alert('Correct!!');
-    catCorrectCount++;
+    if (catNames.includes(nameGuess.toLowerCase())) {
+      alert('Correct!!');
+      catCorrectCount++;
+    }
+    else {
+      alert('Nope - Try Again');
+    }
   }
-  else {
-    alert('Nope - Try Again');
+
+  alert('Correct names of the past and present cats of Noah are: ' + catNames[0] + ' ' + catNames[1] + ' ' + catNames[2] + ' ' + catNames[3]);
+
+  if (catCorrectCount){
+    correctCount++;
   }
 }
 
-alert('Correct names of the past and present cats of Noah are: ' + catNames[0] + ' ' + catNames[1] + ' ' + catNames[2] + ' ' + catNames[3]);
-
-if (catCorrectCount){
-  correctCount++;
-}
+preQa();
+guessNum();
+guessCat();
 
 alert(usrName + ' - You reached the end of the game - Your score was: ' + correctCount + ' of 7.');
